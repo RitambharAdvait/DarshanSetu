70d577fc15a06ab086e4bbcd05f1f2777d00ffe3
Ritambhar Advait <ritambharadvait2007@gmail.com>
2026-07-25 21:34:31 +0530

    Gate validation is added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,7 @@ import dotenv from 'dotenv';
 import { Server as SocketIOServer } from 'socket.io';
 import { MQTTIngestionService } from './services/mqtt.service';
 import authRoutes from './routes/auth.routes';
+import gateRoutes from './routes/gate.routes';
 import ticketRoutes from './routes/ticket.routes';
 
 dotenv.config();
@@ -22,6 +23,8 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 
 app.use('/api/tickets', ticketRoutes);
+
+app.use('/api/gates', gateRoutes);
 
 // Initialize MQTT Ingestion Engine
 new MQTTIngestionService(io);
